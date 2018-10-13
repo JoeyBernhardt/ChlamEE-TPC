99b3abd72a48d7b21b0ec569759e32611401ea81
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-10-13 19:37:30 +0200

    adding more RFU data and scripts for bootstrapping

--- R-scripts/09_RFU_import_globe.R
+++ R-scripts/09_RFU_import_globe.R
@@ -97,13 +97,14 @@ all_rfus3 <- bind_rows(all_rfus2, all_inocs) %>%
 
 
 all_rfus3 %>% 
-	filter(temperature == 16) %>% 
-	# filter(round == "repeat") %>% 
+	# filter(temperature ==28) %>% 
+	filter(round == "repeat") %>% 
 	# filter(population == 1) %>% 
 	ggplot(aes(x = days, y = RFU, color = factor(temperature), group = well_plate)) +
-	geom_point(size = 3) + scale_color_viridis_d(name = "Temperature") + xlab("Days") +
+	geom_point(size = 2) + scale_color_viridis_d(name = "Temperature") + xlab("Days") +
 	facet_wrap( ~ population, scales = "free") +
-	geom_line() + xlim(0, 3)
+	geom_line() +
+	xlim(0, 3)
 ggsave("figures/globe-chlamy-RFU-time.pdf", width = 12, height = 10)
 
 ### which parts are exponential
