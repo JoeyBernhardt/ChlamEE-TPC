0ef050f9dd1ab5847e81ad4443d4074f5869cd2c
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-11-11 11:47:07 +0100

    more data

--- R-scripts/24_chlamee-acclimated-RFU.R
+++ R-scripts/24_chlamee-acclimated-RFU.R
@@ -2,7 +2,8 @@
 
 
 
-### RFU import for GlobeChlamy
+### RFU import for chlamee-acclimated
+
 library(tidyverse)
 library(cowplot)
 library(readxl)
@@ -26,8 +27,7 @@ plate_info <- left_join(plate_layout, plate_key, by = c("plate_key")) %>%
 	mutate(population = ifelse(population == "anc 2", "Anc 2", population))
 
 
-plate_info %>% 
-	filter(is.na(plate_key)) %>% View
+
 
 
 # read in RFU data --------------------------------------------------------
@@ -73,7 +73,7 @@ plate2 <- plate_layout %>%
 	unite(col = well, row, column, sep = "") 
 
 
-str(all_temp_RFU)
+
 
 all_rfus_raw <- left_join(all_temp_RFU, plate_info, by = c("well", "plate")) %>% 
 	mutate(plate = as.numeric(plate)) %>% 
@@ -86,23 +86,21 @@ all_rfus2 <- all_rfus_raw %>%
 	mutate(date_time = ymd_hms(date_time))
 
 all_rfus3 <- all_rfus2 %>% 
-	mutate(round = ifelse(plate %in% c(6, 13, 20), "repeat", "single")) %>% 
+	mutate(round = ifelse(plate %in% c(6, 13, 20, 27, 29, 36, 48, 55, 62, 69, 76, 83), "repeat", "single")) %>% 
+	group_by(temperature) %>% 
 	mutate(start_time = min(date_time)) %>% 
 	mutate(days = interval(start_time, date_time)/ddays(1)) %>% 
 	unite(col = well_plate, well, plate, remove =  FALSE) 
 
 
 all_rfus3 %>%
-	# filter(is.na(temperature)) %>% 
-	# filter(!plate %in% c(37, 38, 39, 40)) %>% 
-	filter(round == "repeat") %>% 
-	filter(temperature == 34) %>% 
-	# filter(population %in% c(1, 2, 5, 6)) %>% 
-	# filter(days < 0.3) %>% 
+	# filter(round == "repeat") %>% 
+	filter(temperature %in% c(34)) %>% 
 	ggplot(aes(x = days, y = RFU, color = factor(temperature), group = well_plate)) +
 	geom_point(size = 2) +
 	scale_color_viridis_d(name = "Temperature") +
 	xlab("Days") +
 	facet_wrap( ~ population, scales = "free_y") +
-	geom_line() 
-ggsave("figures/globe-chlamy-acclimated-RFU-time.pdf", width = 10, height = 8)
+	geom_line(linetype = "dotted") 
+ggsave("figures/chlamee-acclimated-RFU-time.pdf", width = 30, height = 20)
+ggsave("figures/chlamee-acclimated-RFU-time-34C.pdf", width = 14, height = 8)
