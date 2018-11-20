e3758edec390165a7040e829ea10db2af962c8e0
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-11-20 14:35:06 +0100

    adding more fc data from chlamee acclimated

---
+++ R-scripts/13_flow_cytometry_import_chlamee-acclimated.R
@@ -0,0 +1,35 @@
+
+library(tidyverse)
+library(cowplot)
+library(janitor)
+library(tidyverse)
+library(readxl)
+library(stringr)
+library(flowCore)
+
+
+read_fcs <- function(file) {
+	fcs_file <- as.data.frame((exprs(read.FCS(file)))) %>% 
+		clean_names()
+	
+}
+
+
+
+fcs_files_all <- c(list.files("flow-cytometry-data/chlamee-acclimated/",
+							  full.names = TRUE, pattern = ".fcs$", recursive = TRUE))
+
+
+names(fcs_files_all) <- fcs_files_all %>% 
+	gsub(pattern = ".fcs$", replacement = "")
+
+all_fcs_all <- map_df(fcs_files_all, read_fcs, .id = "file_name")
+
+all_fcs_all$file_name[[1]]
+
+all_fcs2_all <- all_fcs_all %>% 
+	separate(file_name, into = c("file_path", "plate_well"), sep = c("-chlamee-acclimated-"), remove = FALSE) %>% 
+	separate(plate_well, into = c("plate", "well"), sep = "/", remove = FALSE)
+
+
+write_csv(all_fcs2_all, "data-processed/chlamee-acclimated-particles.csv")
