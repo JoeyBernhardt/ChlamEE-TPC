c854e052d42523f2b3826aa5361d9fd0ff183170
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-11-21 17:18:28 +0100

    more data

--- R-scripts/13_flow_cytometry_import_dilution_test.R
+++ R-scripts/13_flow_cytometry_import_dilution_test.R
@@ -15,7 +15,7 @@ read_fcs <- function(file) {
 
 
 
-fcs_files_all <- c(list.files("flow-cytometry-data/dilution-tests/",
+fcs_files_all <- c(list.files("flow-cytometry-data/dilution-tests/20181121_162441-high-density-dilution-count-test",
 							  full.names = TRUE, pattern = ".fcs$", recursive = TRUE))
 
 
@@ -27,8 +27,8 @@ all_fcs_all <- map_df(fcs_files_all, read_fcs, .id = "file_name")
 all_fcs_all$file_name[[1]]
 
 all_fcs2_all <- all_fcs_all %>% 
-	separate(file_name, into = c("file_path", "well"), sep = c("-dilution-test/"), remove = FALSE) %>% 
-	mutate(concentration = "low_concentration")
+	separate(file_name, into = c("file_path", "well"), sep = c("-count-test/"), remove = FALSE) %>% 
+	mutate(concentration = "high_concentration")
 
 
-write_csv(all_fcs2_all, "data-processed/dilution-test-low-density-particles.csv")
+write_csv(all_fcs2_all, "data-processed/dilution-test-high-density-particles.csv")
